914ff4a61828624af379500a6231bb5cf02cda0b
Nadipalli-Sandeep <nadipalli.sandeep8@gmail.com>
2024-06-25 18:52:57 +0530

    one

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Zomato Chatbot",
+    version= "0.0.0",
+    author= "Jaya Sandeep Nadipalli",
+    author_email= "nadipalli.sandeep8@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
